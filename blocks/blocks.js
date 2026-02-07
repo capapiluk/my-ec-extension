677b6d80fb631659caadc840e0ed91327b38baa1
CAP_DEV <127816113+capapiluk@users.noreply.github.com>
2026-02-07 21:08:56 +0700

    add

--- blocks/blocks.js
+++ blocks/blocks.js
@@ -1,48 +1,50 @@
-Blockly.defineBlocksWithJsonArray([
-{
-  "type": "ec_read_value",
-  "message0": "read EC value (mS/cm) pin %1",
-  "args0": [
+module.exports = function() {
+    Blockly.defineBlocksWithJsonArray([
     {
-      "type": "input_value",
-      "name": "pin",
-      "check": "Number"
-    }
-  ],
-  "output": "Number",
-  "colour": "#2C97DE",
-  "tooltip": "อ่านค่า EC จาก sensor (mS/cm)",
-  "helpUrl": ""
-},
-{
-  "type": "ec_read_voltage",
-  "message0": "read EC voltage (V) pin %1",
-  "args0": [
+      "type": "ec_read_value",
+      "message0": "read EC value (mS/cm) pin %1",
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "pin",
+          "check": "Number"
+        }
+      ],
+      "output": "Number",
+      "colour": "#2C97DE",
+      "tooltip": "อ่านค่า EC จาก sensor (mS/cm)",
+      "helpUrl": ""
+    },
     {
-      "type": "input_value",
-      "name": "pin",
-      "check": "Number"
-    }
-  ],
-  "output": "Number",
-  "colour": "#2C97DE",
-  "tooltip": "อ่านค่าแรงดัน (Voltage) จาก EC sensor",
-  "helpUrl": ""
-},
-{
-  "type": "ec_set_offset",
-  "message0": "set EC offset %1",
-  "args0": [
+      "type": "ec_read_voltage",
+      "message0": "read EC voltage (V) pin %1",
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "pin",
+          "check": "Number"
+        }
+      ],
+      "output": "Number",
+      "colour": "#2C97DE",
+      "tooltip": "อ่านค่าแรงดัน (Voltage) จาก EC sensor",
+      "helpUrl": ""
+    },
     {
-      "type": "input_value",
-      "name": "offset",
-      "check": "Number"
+      "type": "ec_set_offset",
+      "message0": "set EC offset %1",
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "offset",
+          "check": "Number"
+        }
+      ],
+      "previousStatement": null,
+      "nextStatement": null,
+      "colour": "#2C97DE",
+      "tooltip": "ตั้งค่า offset สำหรับ calibrate EC sensor",
+      "helpUrl": ""
     }
-  ],
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": "#2C97DE",
-  "tooltip": "ตั้งค่า offset สำหรับ calibrate EC sensor",
-  "helpUrl": ""
-}
-]);
+    ]);
+};
--- extension.js
+++ extension.js
@@ -1,3 +1,6 @@
+const blocks = require("./blocks/blocks");
+const generators = require("./blocks/generators");
+
 module.exports = {
     name: "EC Sensor",
     description: "EC (Electrical Conductivity) Sensor for water quality measurement",
@@ -5,5 +8,6 @@ module.exports = {
     category: "Sensor",
     version: "1.0.0",
     icon: "/static/icon.png",
-    color: "#2C97DE"
+    color: "#2C97DE",
+    blocks: blocks
 };

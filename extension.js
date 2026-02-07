51724c41300bfbb743d4a2514de26f7fac78a369
CAP_DEV <127816113+capapiluk@users.noreply.github.com>
2026-02-07 20:58:22 +0700

    edit

--- extension.js
+++ extension.js
@@ -1,48 +1,21 @@
-({
-    name: "EC Sensor",
-    description: "อ่านค่า EC จาก Gravity EC / TDS Sensor (ไม่ชดเชยอุณหภูมิ)",
-    author: "cap_apiluk",
-    category: "Sensors",
-    version: "1.0.0",
-    icon: "/static/icon.png",
-    color: "#2ecc71",
-    blocks: [
-        {
-            xml: `
-                <block type="ec_read_value">
-                    <value name="pin">
-                        <shadow type="math_number">
-                            <field name="NUM">34</field>
-                        </shadow>
-                    </value>
-                </block>
-            `
-        },
-        {
-            xml: `
-                <block type="ec_read_voltage">
-                    <value name="pin">
-                        <shadow type="math_number">
-                            <field name="NUM">34</field>
-                        </shadow>
-                    </value>
-                </block>
-            `
-        },
-        {
-            xml: `
-                <block type="ec_set_offset">
-                    <value name="offset">
-                        <shadow type="math_number">
-                            <field name="NUM">0</field>
-                        </shadow>
-                    </value>
-                </block>
-            `
-        }
-    ],
-    chip: [
-        "ESP32",
-        "Arduino"
-    ]
-});
+class ECsensor {
+    constructor() {
+        this.name = "EC Sensor";
+        this.description = "EC (Electrical Conductivity) Sensor";
+        this.author = "Your Name";
+        this.category = "Sensor";
+        this.version = "1.0.0";
+        this.icon = "/static/icon.png";
+        this.tags = ["EC", "Sensor", "Water Quality"];
+    }
+
+    getBlocks() {
+        return "blocks.js";
+    }
+
+    getGenerators() {
+        return "generators.js";
+    }
+}
+
+module.exports = ECsensor;
